977d8b2f12f88350866daa5d4971c3f98400f075
MajedBannani <65924974+MajedBannani@users.noreply.github.com>
2026-01-15 12:37:32 +0300

    Update r2-media-offloader.php

--- r2-media-offloader.php
+++ r2-media-offloader.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Media Offloader for CF R2
  * Description:       Foundation plugin for offloading WordPress media to CF R2 (S3-compatible storage).
- * Version:           1.0.10
+ * Version:           1.0.11
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * Author:            Majed Talal
